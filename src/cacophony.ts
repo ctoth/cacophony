f0cbb725e33056191b214289f8842efc90a81ab3
Christopher Toth <q.alpha@gmail.com>
2023-08-17 21:43:37 -0600

    Rebuilt removing sounts and better using webaudio

--- src/cacophony.ts
+++ src/cacophony.ts
@@ -1,310 +1,310 @@
-import { SoundSource as BaseSoundSource, SoundSourceOptions, createAudioContext, createSoundListener } from "sounts";
-import { CacheManager } from "./cache";
+import { AudioContext, IAudioBuffer, IAudioBufferSourceNode, IAudioListener, IBiquadFilterNode, IGainNode, IPannerNode } from 'standardized-audio-context';
+import { CacheManager } from './cache';
 
-export enum FadeType {
-    EXPONENTIAL,
-    LINEAR,
+
+type GainNode = IGainNode<AudioContext>;
+type BiquadFilterNode = IBiquadFilterNode<AudioContext>;
+
+
+export interface BaseSound {
+    // the stuff you should be able to do with anything that makes sound including groups, sounds, and playbacks.
+    play(): Playback[];
+    stop(): void;
+    pause(): void;
+    resume(): void;
+    addFilter(filter: BiquadFilterNode): void;
+    removeFilter(filter: BiquadFilterNode): void;
+    moveTo(x: number, y: number, z: number): void;
+}
+export class Cacophony {
+    context: AudioContext;
+    globalGainNode: GainNode;
+    listener: IAudioListener;
+    private prevVolume: number = 1;
+
+    constructor(context?: AudioContext) {
+        this.context = context || new AudioContext();
+        this.listener = this.context.listener;
+        this.globalGainNode = this.context.createGain();
+        this.globalGainNode.connect(this.context.destination);
+    }
+
+    getListener(): IAudioListener {
+        return this.listener;
+    }
+
+    createSound(buffer: AudioBuffer): Sound
+
+    createSound(url: string): Promise<Sound>
+
+    createSound(bufferOrUrl: AudioBuffer | string): Sound | Promise<Sound> {
+        if (bufferOrUrl instanceof AudioBuffer) {
+            return new Sound(bufferOrUrl, this.context, this.globalGainNode);
+        }
+        const url = bufferOrUrl;
+        return CacheManager.getAudioBuffer(url, this.context).then(buffer => new Sound(buffer, this.context, this.globalGainNode));
+    }
+
+    pause() {
+        if ('suspend' in this.context) {
+            this.context.suspend();
+        }
+    }
+
+    resume() {
+        if ('resume' in this.context) {
+            this.context.resume();
+        }
+    }
+
+    stopAll() {
+        if ('close' in this.context) {
+            this.context.close();
+        }
+    }
+
+    setGlobalVolume(volume: number) {
+        this.globalGainNode.gain.value = volume;
+    }
+
+    mute() {
+        this.prevVolume = this.globalGainNode.gain.value;
+        this.setGlobalVolume(0);
+    }
+
+    unmute() {
+        this.setGlobalVolume(this.prevVolume);
+    }
+
 }
 
-interface NodeWrapper {
-    node: AudioNode;
-    next?: NodeWrapper;
+
+export class FilterManager {
+    protected filters: BiquadFilterNode[] = [];
+
+    addFilter(filter: BiquadFilterNode): void {
+        this.filters.push(filter);
+    }
+
+    removeFilter(filter: BiquadFilterNode): void {
+        this.filters = this.filters.filter(f => f !== filter);
+    }
+
+    applyFilters(connection: any): any {
+        this.filters.forEach(filter => {
+            connection.disconnect();
+            connection = connection.connect(filter);
+        });
+        return connection;
+    }
 }
 
-type AudioPosition = {
-    x: number;
-    y: number;
-    z: number;
-};
 
-export abstract class AudioNodeWrapper {
-    protected filters: NodeWrapper[] = [];
-    protected effectsChain: NodeWrapper[] = [];
+class Sound extends FilterManager implements BaseSound {
+    buffer: IAudioBuffer;
+    context: AudioContext;
+    playbacks: Playback[] = [];
+    globalGainNode: GainNode;
+    position: number[] = [0, 0, 0];
 
-    constructor(public soundSource: BaseSoundSource, public context: AudioContext) { }
-
-    get volume() {
-        return this.soundSource.gainNode!.gain.value;
+    constructor(buffer: AudioBuffer, context: AudioContext, globalGainNode: IGainNode<AudioContext>) {
+        super();
+        this.buffer = buffer;
+        this.context = context;
+        this.globalGainNode = globalGainNode;
     }
 
-    set volume(volume: number) {
-        this.soundSource.setGain(volume);
+    preplay(): Playback[] {
+        const source = this.context.createBufferSource();
+        source.buffer = this.buffer;
+        const gainNode = this.context.createGain();
+        source.connect(gainNode);
+        gainNode.connect(this.globalGainNode);
+        const playback = new Playback(source, gainNode, this.context);
+        this.filters.forEach(filter => playback.addFilter(filter));
+        playback.moveTo(this.position[0], this.position[1], this.position[2]);
+        this.playbacks.push(playback);
+        return [playback];
     }
 
-    get position(): AudioPosition {
-        return { x: this.soundSource.node.positionX.value as number, y: this.soundSource.node.positionY.value as number, z: this.soundSource.node.positionZ.value as number }
+    play(): Playback[] {
+        const playback = this.preplay();
+        playback.forEach(p => p.source.start());
+        return playback;
     }
 
-    set position(pos: { x?: number, y?: number, z?: number }) {
-        const position = { ...this.position, ...pos };
-        this.soundSource.setPosition(position.x!, position.y!, position.z!);
+    stop(): void {
+        this.playbacks.forEach(p => p.stop());
     }
 
-    fade(duration: number, fadeType: FadeType = FadeType.EXPONENTIAL, fadeDirection: 'in' | 'out') {
-        const target = this.context.currentTime + duration;
-        const value = fadeDirection === 'in' ? 1 : 0.00001;
-        switch (fadeType) {
-            case FadeType.EXPONENTIAL:
-                this.soundSource.gainNode!.gain.exponentialRampToValueAtTime(value, target);
-                break;
-            case FadeType.LINEAR:
-                this.soundSource.gainNode!.gain.linearRampToValueAtTime(value, target);
-                break;
+    pause(): void {
+        if ('suspend' in this.context) {
+            this.context.suspend();
         }
     }
 
-    fadeIn(duration: number, fadeType: FadeType = FadeType.EXPONENTIAL) {
-        this.fade(duration, fadeType, 'in');
-    }
-
-    fadeOut(duration: number, fadeType: FadeType = FadeType.EXPONENTIAL) {
-        this.fade(duration, fadeType, 'out');
-    }
-
-    getEffectsChain(): AudioNode[] {
-        return this.effectsChain.map(wrapper => wrapper.node);
-    }
-
-    addEffect(node: AudioNode) {
-        this.effectsChain.push({ node });
-    }
-
-    removeEffect(node: AudioNode) {
-        const index = this.filters.findIndex(wrapper => wrapper.node === node);
-        if (index > -1) {
-            this.filters.splice(index, 1);
-            this.rebuildEffectsChain();
+    resume(): void {
+        if ('resume' in this.context) {
+            this.context.resume();
         }
     }
 
-    addFilter(filter: BiquadFilterNode) {
-        this.filters.push({ node: filter });
-        this.rebuildFilterChain();
+    moveTo(x: number, y: number, z: number): void {
+        this.position = [x, y, z];
+        this.playbacks.forEach(p => p.moveTo(x, y, z));
     }
 
-    removeFilter(filter: BiquadFilterNode) {
-        const index = this.filters.findIndex(wrapper => wrapper.node === filter);
-        if (index > -1) {
-            this.filters.splice(index, 1);
-            this.rebuildFilterChain();
+    addFilter(filter: BiquadFilterNode): void {
+        super.addFilter(filter);
+        this.playbacks.forEach(p => p.addFilter(filter));
+    }
+
+    removeFilter(filter: BiquadFilterNode): void {
+        super.removeFilter(filter);
+        this.playbacks.forEach(p => p.removeFilter(filter));
+    }
+}
+
+
+type AudioBufferSourceNode = IAudioBufferSourceNode<AudioContext>;
+type PannerNode = IPannerNode<AudioContext>;
+
+class Playback extends FilterManager implements BaseSound {
+    context: AudioContext;
+    source: AudioBufferSourceNode;
+    gainNode: GainNode;
+    panner: PannerNode;
+
+    constructor(source: AudioBufferSourceNode, gainNode: GainNode, context: AudioContext) {
+        super();
+        this.source = source;
+        this.gainNode = gainNode;
+        this.context = context;
+        this.panner = context.createPanner();
+        source.connect(this.panner).connect(this.gainNode);
+        source.start();
+    }
+
+    play() {
+        this.source.start();
+        return [this];
+    }
+
+    set volume(v: number) {
+        this.gainNode.gain.value = v;
+    }
+
+    fadeIn(time: number): Promise<void> {
+        return new Promise(resolve => {
+            let volume = 0;
+            const increment = this.gainNode.gain.value / (time * 60); // Assuming time is in seconds
+            const interval = setInterval(() => {
+                volume += increment;
+                this.gainNode.gain.value = volume;
+                if (volume >= this.gainNode.gain.value) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 1000 / 60); // 60 times per second
+        });
+    }
+    fadeOut(time: number): Promise<void> {
+        return new Promise(resolve => {
+            let volume = this.gainNode.gain.value;
+            const decrement = this.gainNode.gain.value / (time * 60);
+            const interval = setInterval(() => {
+                volume -= decrement;
+                this.gainNode.gain.value = volume;
+                if (volume <= 0) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 1000 / 60);
+        });
+    }
+
+
+    stop(): void {
+        this.source.stop();
+    }
+
+    pause(): void {
+        if ('suspend' in this.source.context) {
+            this.source.context.suspend();
+        }
+
+    }
+    resume(): void {
+        if ('resume' in this.source.context) {
+            this.source.context.resume();
         }
     }
 
-    protected rebuildFilterChain() {
-        this.connectNodesInChain(this.filters);
+    addFilter(filter: BiquadFilterNode): void {
+        super.addFilter(filter);
+        this.refreshFilters();
     }
 
-    protected rebuildEffectsChain() {
-        this.connectNodesInChain(this.effectsChain);
+    removeFilter(filter: BiquadFilterNode): void {
+        super.removeFilter(filter);
+        this.refreshFilters();
     }
 
-    protected connectNodesInChain(chain: NodeWrapper[]) {
-        chain.forEach((wrapper, index) => {
-            wrapper.node.disconnect();
-            wrapper.next = chain[index + 1];
-            if (wrapper.next) {
-                wrapper.node.connect(wrapper.next.node);
-            } else {
-                wrapper.node.connect(this.context.destination);
+    moveTo(x: number, y: number, z: number): void {
+        this.panner.positionX.value = x;
+        this.panner.positionY.value = y;
+        this.panner.positionZ.value = z;
+    }
+
+    private refreshFilters(): void {
+        let connection = this.source;
+        connection = this.applyFilters(connection);
+    }
+}
+
+
+
+
+class Group implements BaseSound {
+    sounds: Sound[] = [];
+
+    addSound(sound: Sound): void {
+        this.sounds.push(sound);
+    }
+
+    preplay(): Playback[] {
+        return this.sounds.reduce<Playback[]>((playbacks, sound) => playbacks.concat(sound.preplay()), []);
+    }
+
+    play(): Playback[] {
+        return this.sounds.reduce<Playback[]>((playbacks, sound) => playbacks.concat(sound.play()), []);
+    }
+
+    stop(): void {
+        this.sounds.forEach(sound => sound.stop());
+    }
+
+    pause(): void {
+        this.sounds.forEach(sound => sound.pause());
+    }
+
+    resume(): void {
+        this.sounds.forEach(sound => {
+            if ('resume' in sound.context) {
+                sound.context.resume();
             }
         });
     }
 
-    destructor() {
-        this.soundSource.node.disconnect();
-        this.soundSource.gainNode!.disconnect();
-    }
-}
-
-
-export class Sound {
-    private pannerNode?: PannerNode;
-    private soundEffectsChain: AudioNode[] = [];
-
-
-    constructor(
-        public source: AudioSource,
-        private node: AudioBufferSourceNode,
-        context: AudioContext,
-        position?: AudioPosition,
-    ) {
-
-        if (position) {
-            this.pannerNode = context.createPanner();
-            this.pannerNode.positionX.value = position.x;
-            this.pannerNode.positionY.value = position.y;
-            this.pannerNode.positionZ.value = position.z;
-            this.node.connect(this.pannerNode);
-            this.pannerNode.connect(context.destination);
-        } else {
-            this.node.connect(context.destination);
-        }
+    addFilter(filter: BiquadFilterNode): void {
+        this.sounds.forEach(sound => sound.addFilter(filter));
     }
 
-    seek(time: number) {
-        this.node.stop();
-        this.node.start(0, time);
+    removeFilter(filter: BiquadFilterNode): void {
+        this.sounds.forEach(sound => sound.removeFilter(filter));
     }
 
-
-    addEffect(node: AudioNode) {
-        this.soundEffectsChain.push(node);
-    }
-
-    removeEffect(node: AudioNode) {
-        const index = this.soundEffectsChain.indexOf(node);
-        if (index !== -1) {
-            this.soundEffectsChain.splice(index, 1);
-        }
-    }
-
-    play() {
-        const sourceEffectsChain = this.source.getEffectsChain();
-        this.connectNodes(this.node, [...sourceEffectsChain, ...this.soundEffectsChain], this.source.context.destination);
-    }
-
-    protected connectNodes(startNode: AudioNode, chain: AudioNode[], endNode: AudioNode) {
-        chain.unshift(startNode);
-        chain.push(endNode);
-
-        for (let i = 0; i < chain.length - 1; i++) {
-            chain[i].connect(chain[i + 1]);
-        }
-    }
-
-    stop() {
-        this.node.stop();
-        this.source.destructor();
-    }
-
-
-    get looping() {
-        return this.node.loop;
-    }
-
-    set looping(loop: boolean) {
-        this.node.loop = loop;
-    }
-
-    get position() {
-        if (this.pannerNode) {
-            return { x: this.pannerNode.positionX.value as number, y: this.pannerNode.positionY.value as number, z: this.pannerNode.positionZ.value as number }
-        } else {
-            return this.source.position;
-        }
-    }
-
-    set position(pos: AudioPosition) {
-        if (this.pannerNode) {
-            this.pannerNode.positionX.value = pos.x;
-            this.pannerNode.positionY.value = pos.y;
-            this.pannerNode.positionZ.value = pos.z;
-        } else {
-            this.source.position = pos;
-        }
-    }
-
-
-    get playbackRate() {
-        return this.node.playbackRate.value;
-    }
-
-    set playbackRate(rate: number) {
-        this.node.playbackRate.value = rate;
-    }
-
-}
-
-export class AudioSource extends AudioNodeWrapper {
-    private url: string;
-    private group: string;
-
-    constructor(url: string, group: string, context: AudioContext, soundSourceOptions: SoundSourceOptions, public loop: boolean = false, public channel: string = "default") {
-        super(new BaseSoundSource(context.destination, soundSourceOptions), context);
-        this.url = url;
-        this.group = group;
-    }
-
-    async play(position?: AudioPosition): Promise<Sound> {
-        const buffer = await CacheManager.getAudioBuffer(this.url, this.context);
-        const playing = this.soundSource.playOnChannel(this.channel, buffer);
-        return new Sound(this, playing, this.context, position);
-    }
-
-    async playStream() {
-        const audioElement = new Audio(this.url);
-        const mediaElementSource = this.context.createMediaElementSource(audioElement);
-        mediaElementSource.connect(this.soundSource.node);
-        audioElement.play();
-        return audioElement;
-    }
-
-    stop() {
-        this.soundSource.stopAll();
-        this.destructor();
-    }
-}
-
-export class SoundManager {
-
-    private listener = createSoundListener(this.context);
-    private sources: Map<string, AudioSource> = new Map();
-    private soundGroups: Map<string, Set<string>> = new Map();
-
-    constructor(private context: AudioContext = createAudioContext()) { }
-
-    get listenerPosition() {
-        return { x: this.listener.node.positionX.value, y: this.listener.node.positionY.value, z: this.listener.node.positionZ.value }
-    }
-
-    set listenerPosition({ x, y, z }: AudioPosition) {
-        this.listener.setPosition(x, y, z);
-    }
-
-    getSource(url: string, group: string = "default", soundSourceOptions: SoundSourceOptions = {}): AudioSource {
-        let source = this.sources.get(url);
-
-        if (!source) {
-            source = new AudioSource(url, group, this.context, soundSourceOptions);
-            this.sources.set(url, source);
-
-            if (!this.soundGroups.has(group)) {
-                this.soundGroups.set(group, new Set());
-            }
-
-            if (!this.soundGroups.get(group)?.has(url)) {
-                this.soundGroups.get(group)?.add(url);
-            }
-        }
-
-        return source;
-    }
-
-    playSource(url: string, volume: number = 1.0) {
-        const source = this.getSource(url);
-        source.volume = volume;
-        source.play();
-    }
-
-    playStream(url: string, volume: number = 1.0) {
-        const stream = this.getSource(url);
-        stream.volume = volume;
-        stream.playStream();
-    }
-
-    stopAll() {
-        for (const source of this.sources.values()) {
-            source.stop();
-        }
-    }
-
-    stopSoundGroup(group: string) {
-        const soundUrls = this.soundGroups.get(group);
-        if (soundUrls) {
-            for (let url of soundUrls) {
-                const source = this.sources.get(url);
-                source && source.stop();
-            }
-        }
+    moveTo(x: number, y: number, z: number): void {
+        this.sounds.forEach(sound => sound.moveTo(x, y, z));
     }
 }
