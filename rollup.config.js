610fd831d5a758b770755109fff2f055205d303d
Christopher Toth <q.alpha@gmail.com>
2024-01-01 00:36:17 -0500

    Working embedded worklets

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,35 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
-import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import glob from 'glob'; // Ensure you have the 'glob' package installed
 
-export default {
-  input: 'src/processors/phase-vocoder.ts',
+const inputFiles = glob.sync('src/processors/**/*.ts').filter((file) => {
+  // exclude tests and definitions
+  return !file.endsWith('.test.ts') && !file.endsWith('.d.ts');
+});
 
-  output: {
-    file: 'src/bundles/phase-vocoder-bundle.js',
-    format: 'iife',
-  },
+const configs = inputFiles.map(inputFile => {
+  const fileName = inputFile.match(/\/([^\/]+)\.ts$/)[1];
 
-  plugins: [
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    typescript(),
-  ],
-}
+  return {
+    input: inputFile,
+    output: {
+      file: `src/bundles/${fileName}-bundle.js`,
+      format: 'iife',
+    },
+    plugins: [
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      typescript(),
+    ],
+  };
+});
+
+export default configs;
