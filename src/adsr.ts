f68d58e0d27dcb41f0b1418391405b60a30e158c
Talon <talon@iamtalon.me>
2024-07-04 10:14:24 +0200

    Improve envelopes for parameters that aren't 0 to 1

--- src/adsr.ts
+++ src/adsr.ts
@@ -7,34 +7,8 @@ export interface ADSREnvelope {
     release: number;
     sustainLevel: number;
     duration: number;
-}
-
-export function applyADSR(
-    audioParam: IAudioParam,
-    envelope: ADSREnvelope,
-    startTime: number,
-    endTime: number
-): void {
-    const { attack, decay, sustain, release, sustainLevel } = envelope;
-
-    const attackEnd = startTime + attack;
-    const decayEnd = attackEnd + decay;
-    const releaseStart = endTime;
-
-    // Apply attack
-    audioParam.cancelScheduledValues(startTime);
-    audioParam.setValueAtTime(0, startTime);
-    audioParam.linearRampToValueAtTime(1, attackEnd);
-
-    // Apply decay
-    audioParam.linearRampToValueAtTime(sustainLevel, decayEnd);
-
-    // Sustain value should be held until the release phase
-    audioParam.setValueAtTime(sustainLevel, releaseStart);
-
-    // Apply release
-    const releaseEnd = releaseStart + release;
-    audioParam.linearRampToValueAtTime(0, releaseEnd);
+    minValue: number;
+    maxValue: number;
 }
 
 export class ADSR {
@@ -49,7 +23,42 @@ export class ADSR {
         startTime: number,
         duration: number
     ): void {
+        if (!duration || !this.envelope.duration) {
+            // calculate duration based on all envelope properties other than duration itself.
+            duration = this.envelope.attack + this.envelope.decay + this.envelope.release;
+            
+        }
         const endTime = startTime + duration;
-        applyADSR(audioParam, this.envelope, startTime, endTime);
+        this.applyADSR(audioParam, this.envelope, startTime, endTime);
+    }
+
+    private applyADSR(
+        audioParam: IAudioParam,
+        envelope: ADSREnvelope,
+        startTime: number,
+        endTime: number
+    ): void {
+        let { attack, decay, sustain, release, sustainLevel, minValue, maxValue } = envelope;
+        if (!minValue) minValue = 0;
+        if (!maxValue) maxValue = 1;
+    
+        const attackEnd = startTime + attack;
+        const decayEnd = attackEnd + decay;
+        const releaseStart = endTime;
+    
+        // Apply attack
+        audioParam.cancelScheduledValues(startTime);
+        audioParam.setValueAtTime(minValue, startTime);
+        audioParam.linearRampToValueAtTime(maxValue, attackEnd);
+    
+        // Apply decay
+        audioParam.linearRampToValueAtTime(sustainLevel, decayEnd);
+    
+        // Sustain value should be held until the release phase
+        audioParam.setValueAtTime(sustainLevel, releaseStart);
+    
+        // Apply release
+        const releaseEnd = releaseStart + release;
+        audioParam.linearRampToValueAtTime(minValue, releaseEnd);
     }
 }
