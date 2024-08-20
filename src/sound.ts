0319ec2df569cf0b4ce22a71833099fc6a7e61a4
Christopher Toth <q.alpha@gmail.com>
2024-08-19 22:51:24 -0600

    fix: Expose volume property directly on Sound class

--- src/sound.ts
+++ src/sound.ts
@@ -57,7 +57,7 @@ export class Sound
   context: AudioContext;
   loopCount: LoopCount = 0;
   private _playbackRate: number = 1;
-  private _volume: number = 1;
+  _volume: number = 1;
   private eventEmitter: TypedEventEmitter<SoundEvents> =
     new TypedEventEmitter<SoundEvents>();
 
@@ -254,7 +254,7 @@ export class Sound
 
   set volume(volume: number) {
     this._volume = volume;
-    super.volume = volume;
+    this.playbacks.forEach((p) => (p.volume = volume));
     this.emit("volumeChange", volume);
   }
 }
